4085128d1df6194355c11ba6a8685c684603d4ca
Wann-xmh <sasitorn.wann@gmail.com>
2025-07-15 02:36:16 +0000

    add file

---
+++ lecture3/ex00.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("%s\n","Programming");
+    printf("%s\n","Programming");
+    printf("%s\n","Programming");
+    printf("%s\n","Programming");
+    printf("%s\n","Programming");
+    return 0;
+}
---
+++ lecture3/ex02.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    float point;
+    char name[20];
+    printf ("Enter your name : ");
+    scanf ("%s",name);
+    printf ("Enter your point : ");
+    scanf ("%f",&point);
+    return 0;
+}
